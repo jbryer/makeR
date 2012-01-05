ca3d0cbedac7ff9860c6e64909d6ff7eb296a9ff
Jason Bryer <jason@bryer.org>
2012-01-05 09:21:04 -0500

    Initial version.

---
+++ R/ProjectVersion-package.R
@@ -0,0 +1,14 @@
+#' 
+#' 
+#' @name ProjectVersion-package
+#' @aliases ProjectVersion
+#' @docType package
+#' @title Package for managing projects with multiple versions derived from a
+#'        single source repository.
+#' @author \email{jason@@bryer.org}
+#' @keywords institutional research
+#' @import tools
+NULL
+
+.onLoad <- function(libname, pkgname) {
+}
